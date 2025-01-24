4bd29ab7545bc1bf581b72384145a38c4d8e8fec
Luan Silva <luansilvae27@gmail.com>
2025-01-23 23:56:34 -0300

    feat: optimize images

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteImagemin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImagemin({
+    pngquant: {
+      quality: [0.6, 0.8]
+    },
+    mozjpeg: {
+      quality: 75
+    }
+  })],
 })
